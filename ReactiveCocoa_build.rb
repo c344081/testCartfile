999191d19bb01a44bda74a75dd8f67150acf5974
c344081 <45167966@qq.com>
2017-02-24 11:31:56 +0800

    修改脚本
    修改脚本

--- ReactiveCocoa_build.rb
+++ ReactiveCocoa_build.rb
@@ -1,5 +1,5 @@
 p "make up cartfile"
 File.write("Cartfile", 'github "ReactiveCocoa/ReactiveCocoa" ~> 2.5')
-print("hello-----------------------------------")
 p "carthage update"
+`carthage update`
 p "carthage update finish"
